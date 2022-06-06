7bbb69430e1d329daed78578ed2f0fd6be9fe943
Omar Ayman <omaraymankhalaf@gmail.com>
2022-06-06 10:34:26 +0200

    favicon

--- resources/views/admin/admin_master.blade.php
+++ resources/views/admin/admin_master.blade.php
@@ -6,7 +6,7 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<!--favicon-->
-	<link rel="icon" href="{{ asset('backend/assets/images/favicon-32x32.png') }}" type="image/png" />
+	<link rel="icon" href="{{ asset('landing/assets/images/favicon.png') }}" type="image/png" />
 	<!--plugins-->
 
 	<link href="{{ asset('backend/assets/plugins/vectormap/jquery-jvectormap-2.0.2.css') }}" rel="stylesheet"/>
