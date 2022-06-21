0467983edff938b946ac5deb09ba35510990d86d
moatazmujahid99 <forprofmoataz500@gmail.com>
2022-06-22 00:46:07 +0200

    change namespace

--- routes/api/customer.php
+++ routes/api/customer.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\customer\AuthController;
+use App\Http\Controllers\Api\Customer\AuthController;
 use App\Http\Controllers\Api\Customer\CustomerController;
 /*
 |--------------------------------------------------------------------------
